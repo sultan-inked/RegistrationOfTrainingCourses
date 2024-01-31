d9893b3694bb253a11a56eb4dc4e8bf35f3d7638
Sultan <nikowosuka@gmail.com>
2024-01-31 10:58:32 +0600

    ADD: .gitignore (add: .DS_Store .setting/ .classpath .project bin/)

---
+++ src/interfaces/User.java
@@ -0,0 +1,8 @@
+package interfaces;
+import objects.Course;
+public interface User {
+	void showUserInfo();
+	void enrollInCourse(Course course, User user);
+	void unenrollInCourse(Course course, User user);
+	void showEnrollCourseList();
+}
