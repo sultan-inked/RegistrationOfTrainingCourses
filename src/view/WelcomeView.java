6234cf6d8d5615136d88dc5435e6e071d29c071e
Sultan <nikowosuka@gmail.com>
2024-02-08 12:48:07 +0600

    DEL: Delete old version files

--- src/view/WelcomeView.java
+++ src/view/WelcomeView.java
@@ -5,6 +5,7 @@ import view.*;
 
 public class WelcomeView {
 	public static void start() {
+		Alerts.separator();
 		Cnsl.println("Hello there!");
 		Cnsl.println("Just write number with your choice");
 		Cnsl.println("1. Login");
